a14455015f4b978c76aff3901aa891e066b713f8
+ <+>
2023-02-06 18:20:02 +0300

    Assignment Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+
+def greet_programmer
+    puts "Hello, programmer!"
+    
+end
+greet_programmer
+
+def greet name
+   puts "Hello, #{name}!"
+
+end 
+greet ()
+
+
+def greet_with_default(name = "lucy")
+    if name.class === ""
+        puts "Hello, programmer!"
+      end
+
+    puts "Hello, #{name}!"
+ 
+end
+greet_with_default()
+
+def add (num1, num2)
+    num1 + num2
+end 
+
+def halve(num1)
+    if (num1.is_a?  Numeric)
+        num1 / 2;
+        else
+            nil
+        end 
+    end
